18adde001de9d715fbbfeb1dff3b385048afce95
Oriol Fernandez <oriol.developer@gmail.com>
2025-06-18 17:47:07 +0100

    Fix layout error

--- app/src/main/java/com/uriolus/lastparking/presentation/ui/ParkingScreen.kt
+++ app/src/main/java/com/uriolus/lastparking/presentation/ui/ParkingScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
